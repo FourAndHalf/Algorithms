75e0cd807983c8342dd7fd00e9ac37a5880eb6eb
Jinson E B <jinsoneb@gmail.com>
2023-07-12 14:17:29 +0530

    Implementation of insertion sort

--- insertionSort.java
+++ insertionSort.java
@@ -1,3 +1,8 @@
+//*             Insertion Sort             *//
+
+//? Takes the array into a sorted and unsorted part.
+//? Takes one element from unsorted part to sorted part each iteration and places it correctly.
+
 import java.util.Arrays;
 
 public class insertionSort{
@@ -5,14 +10,18 @@ public class insertionSort{
 
     public static Integer[] insertionSorter() {
         int n = testArray.length;
-        for(int i = 0;i < n;i++) {
-            for(int j = 0;j < n;j++) {
-
+        for(int i = 1;i < n;i++) {                              //! Time Complexity = O(n)
+            int current = testArray[i];
+            int j = i-1;
+            while(j >= 0 && current < testArray[j]) {           //! Time Complexity = O(n)
+                testArray[j + 1] = testArray[j];
+                j--;
             }
+            testArray[j + 1] = current;
         }
         return testArray;
-    }
+}                                                               //! Time Complexity = O(n^2)
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(testArray));
+        System.out.println(Arrays.toString(insertionSorter()));
     }
 }
