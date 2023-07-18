0a57241b21ea00a36c323df3379bdcd9edb5bc8b
Jinson E B <jinsoneb@gmail.com>
2023-07-18 09:06:20 +0530

    Re-organized the files

---
+++ NeetCode/Array & Hashing/groupAnagrams.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class groupAnagrams {
+    public static List<List<String>> anagramCalculator(String[] strs) {
+        
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        System.out.println(anagramCalculator(strs));
+    }
+}
