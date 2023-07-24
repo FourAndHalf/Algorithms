b9226d5a2104517e357665f9e1e39a77899b6c0a
Jinson E B <jinsoneb@gmail.com>
2023-07-24 09:28:29 +0530

    Solution to the NeetCode problem Product Array Except Self in java

---
+++ NeetCode/Array & Hashing/productExceptSelf.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class productExceptSelf {
+
+    //* Brute Force Method
+    public static int[] productExceptSelfFinder(int[] nums) {
+        int[] productArray = new int[nums.length];
+        Arrays.fill(productArray, 1);
+        
+        for (int i = 0;i < nums.length;i++) {                                   //! Time Complexity = O(n)
+            for (int j = 0; j < productArray.length; j++) {                     //! Time Complexity = O(n)
+                if(i != j){
+                    productArray[i] = productArray[i] * nums[j];
+                }
+            }
+        }
+
+        return productArray;
+    }                                                                           //! Total Time Complexity = O(n^2)
+
+    //* Another Brute Force Method 
+    public static int[] productExceptSelfFinderII(int[] nums) {
+        int[] productArray = new int[nums.length];
+        int product = 1;
+
+        for(int i = 0;i < nums.length;i++) {                                    //! Time Complexity = O(n)
+            if(nums[i] != 0) {
+                product = product * nums[i];
+            }
+        }
+        Arrays.fill(productArray, product);
+
+        boolean containsZero = false;
+        for(int i = 0;i < productArray.length;i++) {                            //! Time Complexity = O(n)
+            if(nums[i] == 0) {
+                containsZero = true;
+                break;
+            }
+        }
+
+        for(int i = 0;i < nums.length;i++) {                                    //! Time Complexity = O(n)
+            if(containsZero && nums[i] != 0) {
+                productArray[i] = 0;
+            }
+
+            if(!containsZero) {
+                productArray[i] = productArray[i] / nums[i];
+            }
+        }
+
+        return productArray;                                                    
+    }                                                                           //! Total Time Complexity = O(n)
+
+    //* Optimized Solution
+    public static int[] optimizedProductExceptSelfFinder(int[] nums) {
+        int n = nums.length;
+        int[] productArray = new int[n];
+        Arrays.fill(productArray, 1);
+        int curr = 1;
+
+        for(int i = 0;i < n;i++) {                                              //! Time Complexity = O(n)
+            productArray[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for(int i = n-1;i >= 0;i--) {                                           //! Time Complexity = O(n)
+            productArray[i] *= curr;
+            curr *= nums[i];
+        }
+
+        return productArray;
+    }                                                                           //! Total Time Complexity = O(n)
+    public static void main(String[] args) {
+        int[] nums = {-1, 1, 0, -3, 3};
+
+        System.out.println(Arrays.toString(optimizedProductExceptSelfFinder(nums)));
+    }
+}
--- NeetCode/Array & Hashing/topKFrequentElements.java
+++ NeetCode/Array & Hashing/topKFrequentElements.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class topKFrequentElements {
+
+    //* Brute Force Method
     public static int[] frequencyFinder(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
@@ -35,8 +38,28 @@ public class topKFrequentElements {
         }
 
         return array;
-    }                                                                           //! Time Complexity = O(n^2)
-    
+    }                                                                           //! Total Time Complexity = O(n^2)
+
+    //* Heap Method
+    public int[] optimizedFrequencyFinder(int[] nums, int k) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {                                                  //! Time Complexity = O(n)
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> counter.get(b) - counter.get(a));
+        for (int num : counter.keySet()) {                                      //! Time Complexity = O(nlog k)
+            heap.offer(num);
+        }
+        
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {                                           //! Time Complexity = O(k)
+            result[i] = heap.poll();
+        }
+        
+        return result;
+    }                                                                           //! Total Time Complexity = O(nlog k)
+
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 2, 2, 3};
         int k = 2;
