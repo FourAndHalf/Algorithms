88e068e1ec448d454435972cabea0ab3dbbea412
Jinson E B <jinsoneb@gmail.com>
2023-08-15 15:49:09 +0530

    Solution to the NeetCode problem Reverse Linked List in Java

--- NeetCode/Linked List/ReverseLinkedList.java
+++ NeetCode/Linked List/ReverseLinkedList.java
@@ -1,5 +1,5 @@
 public class ReverseLinkedList {
-    public class ListNode {
+    public static class ListNode {
         int value;
         ListNode next;
         ListNode() {}
@@ -12,12 +12,46 @@ public class ReverseLinkedList {
         }
     }   
     
-    public static ListNode reverseList() {
+    //* Iterative Method
+    public static ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode temp;
+
+        while(current != null) {                                //! Time Complexity = O(n)
+            temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
         
-        return reverse;
-    }
+        return previous;
+    }                                                           //! Total Time Complexity = O(n)
+
+    //* Recursive Method
+    public static ListNode recursiveReverseList(ListNode head) {
+        if(head == null) return null;
+
+        ListNode newHead = head;
+        if(head.next != null) {                                 
+            newHead = recursiveReverseList(head.next);
+            head.next.next = head;
+        }
+        head.next = null;
+
+        return newHead;
+    }                                                           //! Total Time Complexity = O(n)
 
     public static void main(String[] args) {
-        
+        int[] values = {1, 2, 3, 4, 5};
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+
+        for(int i = 1;i < values.length;i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        reverseList(head);
     }
 }
