28ead2ae829036dc94b3b213e9e6f41717e95e5f
Jinson E B <jinsoneb@gmail.com>
2023-08-26 13:48:49 +0530

    Have to complete the NeetCode solution for Least Recently Used Cache

---
+++ NeetCode/Linked List/LRUCache.java
@@ -0,0 +1,24 @@
+public class LRUCache {
+    
+    public static class ListNode {
+        int key;
+        int value;
+        ListNode next;
+        ListNode previous;
+    
+        ListNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.previous = null;
+            this.next = null;
+        } 
+    }
+
+    public static void LRUCache(int capacity) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
