9b90f609bc128285344804d6ca0a2b4d96ff8d1d
Jinson E B <jinsoneb@gmail.com>
2023-08-27 23:03:30 +0530

    Solution of the NeetCode Problem for Least Recently Used Cache in Java

---
+++ NeetCode/Linked List/MergeKSortedLists.java
@@ -0,0 +1,24 @@
+public class MergeKSortedLists {
+
+    public class ListNode {
+        int value;
+        ListNode next;
+        ListNode() {}
+        ListNode(int value) {
+            this.value = value;
+        }
+        ListNode(int value, ListNode next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static ListNode sortLists(ListNode[] lists) {
+
+    }
+
+    public static void main(String[] args) {
+        int[][] lists = {{1, 4, 5}, {1, 3, 4}, {2, 6}};
+
+    }
+}
