cbd5ccfca7768840eb8c9ce986a6d65113aa3112
FourAndHalf <jinsoneb@gmail.com>
2025-03-24 06:12:33 +0530

    Sliding window easy problem

--- NeetCode/Sliding Window/BuyAndSellStock.cs
+++ NeetCode/Sliding Window/BuyAndSellStock.cs
@@ -1,16 +1,22 @@
-public class Solution {
-    public int MaxProfit(int[] prices) {
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
         int left = 0;
         int right = 1;
         int max = 0;
         int profit = 0;
 
-        while (right <= prices.Length - 1) {
-            if (prices[left] < prices[right]) {
+        while (right <= prices.Length - 1)
+        {
+            if (prices[left] < prices[right])
+            {
                 profit = prices[right] - prices[left];
-                if (max < profit) 
+                if (max < profit)
                     max = profit;
-            } else {
+            }
+            else
+            {
                 left = right;
             }
 
@@ -18,6 +24,29 @@ public class Solution {
         }
 
         return max;
+    }
+
+    public int MaxProfitSimplified(int[] prices)
+    {
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (int price in prices)
+        {
+            if (price < minPrice)
+            {
+                minPrice = price;
+            }
+
+            int profit = price - minPrice;
+
+            if (profit > maxProfit)
+            {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
     }
 }
 
