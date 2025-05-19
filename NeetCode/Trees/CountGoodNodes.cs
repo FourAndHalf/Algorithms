fb03e39c83ab16422c66968c8dc96a551cc6332c
FourAndHalf <jinsoneb@gmail.com>
2025-05-19 21:20:30 +0530

    Added some solved problems answers

--- NeetCode/Trees/CountGoodNodes.cs
+++ NeetCode/Trees/CountGoodNodes.cs
@@ -1,1 +1,56 @@
-// Implement the solution 
+
+/**
+ * Count Good Nodes in Binary Tree
+ *
+ * Within a binary tree, a node x is considered good if the path from the root of the tree 
+ * to the node x contains no nodes with a value greater than the value of node xGiven
+ *
+ * Given the root of a binary tree root, return the number of good nodes within the tree.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * Takes a value as the highest value in the node, 
+ * Go recursively to the element at the lowest level in the branch
+ * Increase the count of elements on checking if the value is greater then the already shown maxValue
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+*/
+public class Solution {    
+    public int GoodNodes(TreeNode root) {
+        int val = count(root, int.MinValue);
+
+        return val;
+    }
+
+    private int count(TreeNode node, int maxValue) {
+        if (node == null)
+            return 0;
+        
+        int res = (node.val >= maxValue) ? 1 : 0;
+        res += count(node.left, Math.Max(maxValue, node.val));
+        res += count(node.right, Math.Max(maxValue, node.val));
+
+        return res;
+    }
+}
+
--- NeetCode/Trees/KthSmallestElementInBST.cs
+++ NeetCode/Trees/KthSmallestElementInBST.cs
@@ -1,2 +1,55 @@
-// Implement the solution 
-// Code the solution you came up with also
+
+/**
+ * Kth Smallest Integer in BST
+ *
+ * Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) in the tree.
+ * A binary search tree satisfies the following constraints:
+ *      The left subtree of every node contains only nodes with keys less than the node's key.
+ *      The right subtree of every node contains only nodes with keys greater than the node's key.
+ *      Both the left and right subtrees are also binary search trees.
+ *
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * Depth First Search Solution
+ *
+ * Go to the lowest value by recursively going through the lowest left side element
+ * uses the peculiarity of binary search tree
+ *
+*/
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+        List<int> arr = new List<int>();
+        dfs(root, arr);
+
+        return arr[k-1];
+    }
+
+    private void dfs(TreeNode node, List<int> array) {
+        if (node == null)
+            return;
+        
+        dfs(node.left, array);
+        array.Add(node.val);
+        dfs(node.right, array);
+    }
+}
+
